b27c000655b3dd52f9d06e8aea4bfd68d72dc94b
swk <1796050317@qq.com>
2017-08-27 20:26:42 +0800

    设置扫描基础包

--- studySpringWiring/src/main/java/com/swk/autowiring/soundsystem/CDPlayerConfig.java
+++ studySpringWiring/src/main/java/com/swk/autowiring/soundsystem/CDPlayerConfig.java
@@ -4,16 +4,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * 
- * @author 慎伟康
- * 
- * @date 2017年8月27日 下午6:26:30
- * 
- * @Description TODO
- *		启动自动扫描配置文件
+ * 启动自动扫描配置文件
  */
 @Configuration
-@ComponentScan//启用了组件扫描
+@ComponentScan(basePackageClasses=Marker.class)//@ComponentScan(basePackages="com.swk.autowiring.soundsystem")//启用了组件扫描
 public class CDPlayerConfig {
 
 }
