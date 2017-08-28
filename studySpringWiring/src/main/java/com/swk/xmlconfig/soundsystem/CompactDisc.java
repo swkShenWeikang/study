5121e15be714cc4dfe5df9bde41eed29adcff085
swk <1796050317@qq.com>
2017-08-28 19:36:29 +0800

    CD接口和播放器接口

---
+++ studySpringWiring/src/main/java/com/swk/xmlconfig/soundsystem/CompactDisc.java
@@ -0,0 +1,8 @@
+package com.swk.xmlconfig.soundsystem;
+
+/**
+ * CD接口
+ */
+public interface CompactDisc {
+	public void play();
+}
---
+++ studySpringWiring/src/main/java/com/swk/xmlconfig/soundsystem/MediaPlayer.java
@@ -0,0 +1,10 @@
+package com.swk.xmlconfig.soundsystem;
+
+/**
+ * 媒体播放器接口
+ */
+public interface MediaPlayer {
+	
+	public void play();
+
+}
